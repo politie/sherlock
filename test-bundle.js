e4af8c2eb4c99b9351471e17076086f18ea8255c
Paco van der Linden <pavadeli@gmail.com>
2017-12-08 10:36:02 +0100

    added bundle test

---
+++ test-bundle.js
@@ -0,0 +1,19 @@
+console.log('Testing bundle...');
+
+const sherlock = require('./dist/sherlock/sherlock.cjs');
+const assert = require('assert');
+
+const a$ = sherlock.atom({ nested: { property: 'value' } });
+
+let receivedValue
+a$.react(v => receivedValue = v, { skipFirst: true });
+
+assert.strictEqual(receivedValue, undefined);
+
+const value$ = a$.pluck('nested').pluck('property');
+assert.strictEqual(value$.get(), 'value');
+value$.set('another value');
+
+assert.deepStrictEqual(receivedValue, { nested: { property: 'another value' } });
+
+console.log('Bundle ok.');
