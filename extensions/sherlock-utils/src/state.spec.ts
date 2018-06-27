fa31fd18e18b44b34c13901fc30160f78c549575
Paco van der Linden <pavadeli@gmail.com>
2018-06-27 14:47:00 +0200

    Adds state (de)materialization to utils

---
+++ extensions/sherlock-utils/src/state.spec.ts
@@ -0,0 +1,169 @@
+import { _internal, atom, constant } from '@politie/sherlock';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import { copyState, dematerialize, getState, materialize, setState, StateObject, syncState } from './state';
+
+describe('sherlock-utils/copyState', () => {
+    it('should transfer value state', () => {
+        const from$ = constant(123);
+        const to$ = atom.unresolved();
+        copyState(from$, to$);
+        expect(to$.get()).to.equal(123);
+    });
+
+    it('should transfer unresolved state', () => {
+        const from$ = constant.unresolved();
+        const to$ = atom(123);
+        copyState(from$, to$);
+        expect(to$.resolved).to.be.false;
+    });
+
+    it('should transfer error state', () => {
+        const from$ = constant.error('womp womp');
+        const to$ = atom(123);
+        copyState(from$, to$);
+        expect(to$.error).to.equal('womp womp');
+    });
+});
+
+describe('sherlock-utils/getState', () => {
+    it('should return the state as a StateObject', () => {
+        const a$ = atom(undefined);
+        expect(getState(a$)).to.deep.equal({ value: undefined, errored: false, resolved: true });
+        a$.setError(undefined);
+        expect(getState(a$)).to.deep.equal({ error: undefined, errored: true, resolved: true });
+        a$.unset();
+        expect(getState(a$)).to.deep.equal({ errored: false, resolved: false });
+    });
+
+    it('should not call the internal getter more than once', () => {
+        const a$ = atom(undefined);
+        const getter = spy(a$ as any as _internal.BaseDerivable<any>, _internal.symbols.getState);
+        getState(a$);
+        expect(getter).to.have.been.calledOnce;
+        a$.setError(undefined);
+        getState(a$);
+        expect(getter).to.have.been.calledTwice;
+        a$.unset();
+        getState(a$);
+        expect(getter).to.have.been.calledThrice;
+    });
+});
+
+describe('sherlock-utils/materialize', () => {
+    it('should materialize all possible states', () => {
+        const a$ = atom(0);
+        const unresolved$ = constant.unresolved<number>();
+        const error = new Error('this is not good!');
+        const d$ = a$.derive(v => {
+            switch (v) {
+                case 0: return 42;
+                case 1: throw error;
+                default: return unresolved$.get() + 42;
+            }
+        });
+        let reactions = 0;
+        let state: StateObject<number> | undefined;
+        const done = materialize(d$).react(newState => (state = newState, ++reactions));
+
+        expect(reactions).to.equal(1);
+        expect(state).to.deep.equal({ value: 42, errored: false, resolved: true });
+
+        a$.set(1);
+
+        expect(reactions).to.equal(2);
+        expect(state).to.deep.equal({ error, errored: true, resolved: true });
+
+        a$.set(2);
+
+        expect(reactions).to.equal(3);
+        expect(state).to.deep.equal({ errored: false, resolved: false });
+
+        a$.set(0);
+
+        expect(reactions).to.equal(4);
+        expect(state).to.deep.equal({ value: 42, errored: false, resolved: true });
+
+        done();
+    });
+});
+
+describe('sherlock-utils/dematerialize', () => {
+    it('should dematerialize all possible states', () => {
+        const a$ = atom<StateObject<number>>({ errored: false, resolved: false });
+        const d$ = dematerialize(a$);
+        const error = new Error('this is not good!');
+
+        expect(d$.resolved).to.be.false;
+
+        a$.set({ value: 42, errored: false, resolved: true });
+
+        expect(d$.value).to.equal(42);
+
+        a$.set({ error, errored: true, resolved: true });
+
+        expect(d$.error).to.equal(error);
+
+        a$.set({ errored: false, resolved: false });
+
+        expect(d$.resolved).to.be.false;
+
+        a$.set({ value: 42, errored: false, resolved: true });
+
+        expect(d$.value).to.equal(42);
+    });
+});
+
+describe('sherlock-utils/setState', () => {
+    it('should set value state', () => {
+        const to$ = atom.unresolved();
+        setState(to$, { value: 123, errored: false, resolved: true });
+        expect(to$.get()).to.equal(123);
+    });
+
+    it('should set unresolved state', () => {
+        const to$ = atom(123);
+        setState(to$, { errored: false, resolved: false });
+        expect(to$.resolved).to.be.false;
+    });
+
+    it('should set error state', () => {
+        const to$ = atom(123);
+        setState(to$, { error: 'womp womp', errored: true, resolved: true });
+        expect(to$.error).to.equal('womp womp');
+    });
+});
+
+describe('sherlock-utils/syncState', () => {
+    it('should sync all possible states between the two Derivables', () => {
+        const a$ = atom(0);
+        const unresolved$ = constant.unresolved<number>();
+        const error = new Error('this is not good!');
+        const d$ = a$.derive(v => {
+            switch (v) {
+                case 0: return 42;
+                case 1: throw error;
+                default: return unresolved$.get() + 42;
+            }
+        });
+        const target$ = atom.unresolved<number>();
+
+        const done = syncState(d$, target$);
+
+        expect(target$.value).to.equal(42);
+
+        a$.set(1);
+
+        expect(target$.error).to.equal(error);
+
+        a$.set(2);
+
+        expect(target$.resolved).to.be.false;
+
+        a$.set(0);
+
+        expect(target$.value).to.equal(42);
+
+        done();
+    });
+});
---
+++ extensions/sherlock-utils/src/state.ts
@@ -0,0 +1,52 @@
+import { _internal, Derivable, DerivableAtom, derive, ReactorOptions, SettableDerivable } from '@politie/sherlock';
+
+export type StateObject<V> =
+    { value: V, error?: undefined, errored: false, resolved: true } |
+    { value?: undefined, error: any, errored: true, resolved: true } |
+    { errored: false, resolved: false };
+
+export function getState<V>(from: Derivable<V>): StateObject<V> {
+    const state = from[_internal.symbols.getState]();
+    if (state === _internal.symbols.unresolved) {
+        return { errored: false, resolved: false };
+    }
+    if (state instanceof _internal.ErrorWrapper) {
+        const { error } = state;
+        return { error, errored: true, resolved: true };
+    }
+    return { value: state, errored: false, resolved: true };
+}
+
+export function materialize<V>(derivable: Derivable<V>): Derivable<StateObject<V>> {
+    return derive(() => getState(derivable));
+}
+
+export function dematerialize<V>(derivable: Derivable<StateObject<V>>): Derivable<V> {
+    return derivable.derive(state => {
+        if (state.errored) {
+            return new _internal.ErrorWrapper(state.error);
+        }
+        if (state.resolved) {
+            return state.value;
+        }
+        return _internal.symbols.unresolved;
+    });
+}
+
+export function setState<V>(to: SettableDerivable<V> & DerivableAtom, state: StateObject<V>) {
+    if (!state.resolved) {
+        to.unset();
+    } else if (state.errored) {
+        to.setError(state.error);
+    } else {
+        to.set(state.value);
+    }
+}
+
+export function syncState<V>(from: Derivable<V>, to: SettableDerivable<V> & DerivableAtom, opts?: ReactorOptions<StateObject<V>>) {
+    return materialize(from).react(state => setState(to, state), opts);
+}
+
+export function copyState<V>(from: Derivable<V>, to: SettableDerivable<V> & DerivableAtom) {
+    setState(to, getState(from));
+}
