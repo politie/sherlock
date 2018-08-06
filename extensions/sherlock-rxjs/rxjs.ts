3153093013d504127bec49004d27fb115ec57ad4
Wouter Spaak <w.spaak@gmail.com>
2018-08-06 09:20:45 +0200

    Exports toObservable as an independent function (#109)
    The `Derivable` prototype is no longer monkey-patched with a `toObservable` method.  `toObservable` is now a function that accepts two arguments: the `Derivable` to turn into an `Observable` and optional `ReactorOptions`.

--- extensions/sherlock-rxjs/rxjs.ts
+++ extensions/sherlock-rxjs/rxjs.ts
@@ -1,34 +1,21 @@
 import { _internal, atom, Derivable, ReactorOptions } from '@politie/sherlock';
 import { Observable, Subscriber, Subscription } from 'rxjs';
 
-// Adds the toObservable method to Derivable.
-declare module '@politie/sherlock/derivable/extension' {
-    export interface DerivableExtension<V> {
-        /**
-         * Create an RxJS Observable from this Derivable. The Observable stream can be tweaked with the same options that
-         * can also be used with {@link Derivable#react}.
-         *
-         * @param options optional options that indicate how to transform the Derivable into an Observable stream
-         */
-        toObservable(options?: Partial<ReactorOptions<V>>): Observable<V>;
-    }
-}
-
-_internal.BaseDerivable.prototype.toObservable = function toObservable<V>(
-    this: _internal.BaseDerivable<V>,
-    options?: Partial<ReactorOptions<V>>,
-) {
+/**
+ * Creates an RxJS Observable from a Derivable. Optionally accepts a `ReactorOptions` that governs RxJS emissions
+ * and lifecycle equivalent to {@link Derivable#react} {@link ReactorOptions}.
+ * @param derivable Derivable to create an RxJS Observable from.
+ * @param options Partial `ReactorOptions`.
+ */
+export function toObservable<V>(derivable: Derivable<V>, options?: Partial<ReactorOptions<V>>): Observable<V> {
     return new Observable<V>((subscriber: Subscriber<V>) => {
-        return _internal.Reactor.create(this,
-            // onValue: notify subscriber
+        return _internal.Reactor.create(derivable as _internal.BaseDerivable<V>,
             value => subscriber.next(value),
-            // Merge the options with the default options.
             options,
-            // onComplete: notify subscriber unless explicitly unsubscribed
             () => subscriber.closed || subscriber.complete(),
         );
     });
-};
+}
 
 export function fromObservable<V>(observable: Observable<V>): Derivable<V> {
     const atom$ = atom.unresolved<V>();
