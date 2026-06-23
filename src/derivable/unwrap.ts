54556ca1104fb3c9952770de69ec5cebd6dfb866
Emiel Beeksma <emiel.beeksma@politie.nl>
2026-06-23 15:46:06 +0200

    update pipelines and dependencies

--- src/derivable/unwrap.ts
+++ src/derivable/unwrap.ts
@@ -1,5 +1,15 @@
-import { Unwrappable } from '../interfaces';
+import { Derivable, Unwrappable } from '../interfaces';
 import { BaseDerivable } from './base-derivable';
+
+/**
+ * Determines whether an Unwrappable `v` is a Derivable.
+ *
+ * @param v a value or derivable
+ * @returns whether v is a Derivable
+ */
+function isDerivable<T>(v: Unwrappable<T>): v is Derivable<T> {
+    return v instanceof BaseDerivable;
+}
 
 /**
  * Unwraps a derivable or does nothing if `v` is not a derivable.
@@ -7,9 +17,9 @@ import { BaseDerivable } from './base-derivable';
  * @param v a value or derivable
  */
 export function unwrap<T>(v: Unwrappable<T>): T {
-    return v instanceof BaseDerivable ? v.get() : v;
+    return isDerivable(v) ? v.get() : v;
 }
 
 export function safeUnwrap<T>(v: Unwrappable<T>): T | undefined {
-    return v instanceof BaseDerivable ? v.value : v;
+    return isDerivable(v) ? v.value : v;
 }
